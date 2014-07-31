840352ff1324061fb2c56229ef313d83d086bbbb
Alan Shaw <alan@nodename.com>
2014-07-30 17:08:14 -0700

    Updated description.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject edge-algebra "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An implementation of
+  Guibas, L. and J. Stolfi. 1985. \"Primitives for the Manipulation of
+  General Subdivisions and the Computation of Voronoi Diagrams,\"
+  ACM Transactions on Graphics 4(2):74-123."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
